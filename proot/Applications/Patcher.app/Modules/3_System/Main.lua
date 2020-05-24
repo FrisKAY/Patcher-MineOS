77d69f75d2aa3505d75b1d354f854123988ce551
Alexander <makst79@gmail.com>
2020-05-24 23:54:31 +0300

    system reload

--- proot/Applications/Patcher.app/Modules/3_System/Main.lua
+++ proot/Applications/Patcher.app/Modules/3_System/Main.lua
@@ -2,70 +2,35 @@
 local GUI = require("GUI")
 local paths = require("Paths")
 local system = require("System")
+local fs = require("filesystem")
+local component = require("component")
+local image = require("Image")
+local EFI = component.eeprom
+local totalMemory = math.modf(computer.totalMemory() / 1024)
+local freeMemory = math.modf(computer.freeMemory() / 1024)
+local address = computer.address()
+local cache = fs.path(system.getCurrentScript())
+local lvlenergy = math.modf(computer.energy())
+local maxenergy = math.modf(computer.maxEnergy())
+local uptime = math.modf(computer.uptime())
+local efiname = EFI.getLabel()
+local boot = EFI.getData()
 
 local module = {}
 
 local workspace, window, localization = table.unpack({...})
-local userSettings = system.getUserSettings()
 
 --------------------------------------------------------------------------------
 
 module.name = localization.system
 module.margin = 3
 module.onTouch = function()
-	window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.systemArchitecture))
-
-	local CPUComboBox = window.contentLayout:addChild(GUI.comboBox(1, 1, 36, 3, 0xE1E1E1, 0x696969, 0xD2D2D2, 0xA5A5A5))
-	local architectures, architecture = computer.getArchitectures(), computer.getArchitecture()
-	for i = 1, #architectures do
-		CPUComboBox:addItem(architectures[i]).onTouch = function()
-			computer.setArchitecture(architectures[i])
-			computer.shutdown(true)
-		end
-
-		if architecture == architectures[i] then
-			CPUComboBox.selectedItem = i
-		end
-	end
-
-	window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, localization.systemRAM))
-
-	local RAMComboBox = window.contentLayout:addChild(GUI.comboBox(1, 1, 36, 3, 0xE1E1E1, 0x696969, 0xD2D2D2, 0xA5A5A5))
-	RAMComboBox.dropDownMenu.itemHeight = 1
-
-	local function update()
-		local libraries = {}
-		for key, value in pairs(package.loaded) do
-			if _G[key] ~= value then
-				table.insert(libraries, key)
-			end
-		end
-		
-		table.sort(libraries, function(a, b) return unicode.lower(a) < unicode.lower(b) end)
-
-		RAMComboBox:clear()
-		for i = 1, #libraries do
-			RAMComboBox:addItem(libraries[i])
-		end
-
-		workspace:draw()
-	end
-
-	window.contentLayout:addChild(GUI.button(1, 1, 36, 3, 0xE1E1E1, 0x696969, 0x696969, 0xE1E1E1, localization.systemUnload)).onTouch = function()
-		package.loaded[RAMComboBox:getItem(RAMComboBox.selectedItem).text] = nil
-		update()
-	end
-
-	local switch = window.contentLayout:addChild(GUI.switchAndLabel(1, 1, 36, 8, 0x66DB80, 0xE1E1E1, 0xFFFFFF, 0xA5A5A5, localization.systemUnloading .. ":", userSettings.packageUnloading)).switch
-	switch.onStateChanged = function()
-		userSettings.packageUnloading = switch.state
-		system.setPackageUnloading(userSettings.packageUnloading)
-		system.saveUserSettings()
-	end
-
-	window.contentLayout:addChild(GUI.textBox(1, 1, 36, 1, nil, 0xA5A5A5, {localization.systemInfo}, 1, 0, 0, true, true))
-
-	update()
+   window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, freeMemory .." КБ из ".. totalMemory .."КБ свободно"))
+   window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, lvlenergy .. " из ".. maxenergy .." энегрии"))
+   window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, uptime .. " сек. работы компьютера"))
+   window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, efiname .. " - Ваш EFI"))
+   window.contentLayout:addChild(GUI.text(1, 1, 0x2D2D2D, boot .. " - Диск с ОС"))
+   
 
 	workspace:draw()
 end
